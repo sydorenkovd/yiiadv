9a1e6f5c611639439f6ac533eea4dcc8cacb3771
sydorenkovd <sydorenkovd@gmail.com>
2016-02-13 21:53:01 +0200

    crud search

---
+++ backend/models/PostSearch.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use frontend\models\Posts;
+
+/**
+ * PostSearch represents the model behind the search form about `frontend\models\Posts`.
+ */
+class PostSearch extends Posts
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['title', 'description', 'create_date', 'image', 'author', 'tags', 'is_moderate'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Posts::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'create_date' => $this->create_date,
+            'author_id' => $this->author_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'image', $this->image])
+            ->andFilterWhere(['like', 'author', $this->author])
+            ->andFilterWhere(['like', 'tags', $this->tags])
+            ->andFilterWhere(['like', 'is_moderate', $this->is_moderate]);
+
+        return $dataProvider;
+    }
+}
---
+++ backend/views/posts/_search.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\PostSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="posts-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'title') ?>
+
+    <?= $form->field($model, 'description') ?>
+
+    <?= $form->field($model, 'create_date') ?>
+
+    <?= $form->field($model, 'image') ?>
+
+    <?php // echo $form->field($model, 'author') ?>
+
+    <?php // echo $form->field($model, 'tags') ?>
+
+    <?php // echo $form->field($model, 'is_moderate') ?>
+
+    <?php // echo $form->field($model, 'author_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
