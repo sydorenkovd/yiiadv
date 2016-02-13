73e5ce54f7e56477fbd67884ff5e03093f9ae543
sydorenkovd <sydorenkovd@gmail.com>
2016-02-13 23:24:15 +0200

    bootstrap id debug errror

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,11 +11,7 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'module' => [
-//        'settings' => [
-//            'class' => 'backend\modules\settings\Settings',
-//        ],
-    ],
+    'module' => [],
     'components' => [
         'user' => [
             'identityClass' => 'common\models\User',
