37990904f6b50f14c47c47796db67513b6e21b2a
sydorenkovd <sydorenkovd@gmail.com>
2016-03-17 13:13:00 +0200

    part of migrations

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -45,6 +45,80 @@ class m130524_201442_init extends Migration
             'create_date' => $this->integer()->notNull()
 
         ], $tableOptions);
+        $this->createTable('auth_assignment', [
+            'item_name' => $this->string(64)->notNull(),
+            'user_id' => $this->integer()->notNull(),
+            'created_at' => $this->dateTime()->defaultExpression('CURRENT_TIMESTAMP')
+        ]);
+    $this->createTable('auth_item', [
+       'name' => $this->string(64)->notNull(),
+        'type' => $this->integer()->notNull(),
+        'description' => $this->text(),
+        'rule_name' => $this->string(64)->defaultValue(null),
+        'data' => $this->text(),
+        'created_at' => $this->integer()->defaultValue(null),
+        'updated_at' => $this->integer()->defaultValue(null)
+    ]);
+        $this->createTable('auth_item_child', [
+           'parent' => $this->string(64)->notNull(),
+            'child' => $this->string(64)->notNull()
+        ]);
+        $this->createTable('auth_rule', [
+           'name' => $this->string(64)->notNull(),
+            'data' => $this->text()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull()
+        ]);
+        $this->createTable('customer', [
+           'id' => $this->integer()->notNull(),
+            'name' => $this->string(50)->notNull(),
+            'surname' => $this->string(50)->notNull(),
+            'phone_number' => $this->string(50)->notNull()
+        ]);
+        $this->createTable('event', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull(),
+            'description' => $this->text()->notNull(),
+            'create_date' => $this->dateTime()->defaultExpression('CURRENT_TIMESTAMP')
+        ]);
+        $this->createTable('migration', [
+           'version' => $this->string(180)->notNull(),
+            'apply_time' => $this->integer()->notNull()
+        ]);
+        $this->createTable('po', [
+           'id' => $this->primaryKey(),
+            'po_no' => $this->string(20)->notNull(),
+            'description' => $this->text()->notNull()
+        ]);
+        $this->createTable('po_item', [
+            'id' => $this->primaryKey(),
+            'po_item_no' => $this->string(10)->notNull(),
+            'quantity' => $this->double()->notNull(),
+            'po_id' => $this->primaryKey()
+        ]);
+        $this->createTable('reservation', [
+            'id' => $this->primaryKey(),
+            'room_id' => $this->integer()->notNull(),
+            'customer_is' => $this->integer()->notNull(),
+            'price_per_day' => $this->decimal(20,2)->notNull(),
+            'date_from' => $this->dateTime()->notNull(),
+            'date_to' => $this->dateTime()->notNull(),
+            'reservation_date' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP')
+        ]);
+        $this->createTable('room', [
+            'id' => $this->primaryKey(),
+            'floor' => $this->integer()->notNull(),
+            'room_number' => $this->integer()->notNull(),
+            'has_conditioner' => $this->integer(1)->notNull(),
+            'has_tv' => $this->integer(1)->notNull(),
+            'has_phone' => $this->integer(1)->notNull(),
+            'available_from' => $this->dateTime()->defaultExpression('CURRENT_TIMESTAMP')->notNull(),
+            'price_per_day' => $this->decimal(20,2)->defaultValue(null),
+            'description' => $this->text()->notNull()
+        ]);
+        $this->createTable('', [
+
+        ]);
     }
 
     public function down()
